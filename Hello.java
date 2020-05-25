0385298e0fef23a52689adbee65cc9f840c5cb93
Kris <rkvintha@yahoo.com>
2020-05-24 18:47:49 -0700

    updte to Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@
 public class Hello
 {
 	public static void main(String[] args) {
-		for(int i=1;i<=10;i++) {
+		for(int i=1;i<=2;i++) {
 		System.out.println("Hello Kris --- " +i);
 		System.out.println("SUCCESS THRU GITHUB COMMIT");
 		}
